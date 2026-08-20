3a1bfaf926fa4967e65ac60445ee7cd3910e33c4
konstantin <konstantineremkin25@gmail.com>
2026-08-20 17:15:54 +0700

    Bug fixes, code cleanup

--- tournament.py
+++ tournament.py
@@ -1,10 +1,9 @@
-import math
 from enum import Enum
 from random import shuffle
 from typing import List, Self
 
 class Status(Enum):
-    IN_PROGRESS = "in_progress",
+    IN_PROGRESS = "in_progress"
     FINISHED = "finished"
 
 class Match:
@@ -30,10 +29,6 @@ class Match:
                 f' Team_2: {self.team2}; Status: {self.status}; Next_Match_id: {next_match_id}; Teams_count: {self.teams_count})')
 
 
-def bye(bye_match: Match):
-    bye_match.next_match.team2 = bye_match.team1
-
-
 def create_bracket(teams: List[str]) -> List[Match]:
     if len(teams) < 2:
         return []
@@ -41,16 +36,12 @@ def create_bracket(teams: List[str]) -> List[Match]:
     shuffle(teams)
     teams_count = len(teams)
     bracket = []
-    rounds = []
-    bye_rounds = []
     extra_team = None
 
     curr_round = 1
     while teams_count > 1:
         matches_in_round = teams_count // 2
         bye_teams = teams_count % 2
-        bye_rounds.append(bye_teams)
-        round_array = []
 
         if bye_teams:
             if len(teams) > 1:
@@ -69,9 +60,6 @@ def create_bracket(teams: List[str]) -> List[Match]:
                     match.teams_count = 1
                     extra_team = None
             bracket.append(match)
-            round_array.append(match)
-
-        rounds.append(round_array)
         teams_count = matches_in_round + bye_teams
         curr_round += 1
 
@@ -88,9 +76,9 @@ def create_bracket(teams: List[str]) -> List[Match]:
     return bracket
 
 
-def handle_result(bracket: List[Match], winner: str):
+def handle_result(bracket: List[Match], winner: str) -> None:
     for match in bracket:
-        if match.team1 == winner or match.team2 == winner and match.status == Status.IN_PROGRESS:
+        if (match.team1 == winner or match.team2 == winner) and match.status == Status.IN_PROGRESS:
             if match.next_match.team1 is None:
                 match.next_match.team1 = winner
                 match.status = Status.FINISHED
@@ -106,10 +94,10 @@ def print_bracket(bracket):
     print()
 
 
-teams1 = ["A", "B", "C", "D", "E", "F"]
+teams1 = ["A", "B", "C", "D", "E"]
 bracket1 = create_bracket(teams1)
 print_bracket(bracket1)
-# handle_result(bracket1, bracket1[0].team1)
-# print_bracket(bracket1)
-# handle_result(bracket1, bracket1[1].team2)
-# print_bracket(bracket1)
+handle_result(bracket1, bracket1[0].team1)
+print_bracket(bracket1)
+handle_result(bracket1, bracket1[1].team2)
+print_bracket(bracket1)
